fb2ebbd1ef8f088dc776928708d6a6de3742877d
Mathew Ross <32267136+Nyarry@users.noreply.github.com>
2018-02-09 13:59:28 +0000

    Refactored

--- www/js/index.js
+++ www/js/index.js
@@ -1,60 +1,7 @@
 var app = {
-	counts: {
-		paused: 0,
-		resumed: 0,
-		launched: 0
-	},
-	storage: {
-		data: {
-			["Chair"]: "Mahogany",
-			["Table"]: "Oak",
-			["Shelf"]: "Maple",
-			["Wardrobe"]: "Pine",
-			["Gazeebo"]: "Vaneer"
-		}
-	},
-	display: {},
-	helper: {},
-}
-
-document.addEventListener("deviceready", app.onDeviceReady, false);
-
-app.onDeviceReady = function() {
-	console.log("onDeviceReady");
-
-	document.addEventListener("resume", app.onResume, false);
-	document.addEventListener("pause", app.onPause, false);
-
-	app.counts.launched ++;
-	app.updateDisplay();
-	app.storage.setupKeys();
-}
-
-app.updateDisplay = function() {
-	console.log("updateDisplay");
-	$("#paused").text("Resumed: " + app.counts.paused);
-	$("#resumed").text("Paused: " + app.counts.resumed);
-	$("#launched").text("Launched: " + app.counts.launched);
-}
-
-app.onPause = function() {
-	console.log("onPause");
-	app.counts.paused ++;
-	app.updateDisplay();
-}
-
-app.onResume = function() {
-	console.log("onResume");
-	app.counts.resumed ++;
-	app.updateDisplay();
-}
-
-app.storage.setupKeys = function() {
-	Object.keys(app.storage.data).forEach(function(key, index) {
-		var value = app.storage.data[key]
-		app.storage.setItem(key, value);
-		console.log("setupKeys: " + key + " -> " + value);
-	});
+	storage: {},
+	events: {},
+	display: {}
 }
 
 app.storage.setItem = function(key, value) {
@@ -76,17 +23,29 @@ app.storage.getItemFromIndex = function(index) {
 	return {key, value}
 }
 
-app.display.updateText = function() {
-	window.setInterval(function() {
-		var index = Math.floor(Math.random() * window.localStorage.length);
-		var data = app.storage.getItemFromIndex(index);
+app.events.onDeviceReady = function() {
+	console.log("app.events.onDeviceReady");
+	app.display.update();
+}
 
-		$("#item").text(data.key);
-		$("#typeOfWood").text(data.value);
-	}, 1000);
+app.events.onPause = function() {
+	console.log("app.events.onPause");
+	app.display.update();
+}
+
+app.events.onResume = function() {
+	console.log("app.events.onResume");
+	app.updateDisplay();
+}
+
+app.display.update = function() {
+	console.log("app.display.update");
 }
 
 window.onload = function() {
-	app.onDeviceReady();
-	app.display.updateText();
+	document.addEventListener("deviceready", app.events.onDeviceReady, false);
+	document.addEventListener("resume", app.events.onResume, false);
+	document.addEventListener("pause", app.events.onPause, false);
+
+	app.events.onDeviceReady();
 }
